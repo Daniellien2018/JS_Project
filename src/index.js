446a7e53ae03efd4f2e9235a49477ffaa74382b0
daniellien2018 <daniellien2018@gmail.com>
2022-08-12 15:39:17 -0700

    csv-data-added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// const View = require('./ttt-view.js')// require appropriate file
+// const Game = require('../ttt_node/game.js')// require appropriate file
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Your code here
+//   window.Game = Game;
+//   let el = document.querySelector(".ttt");
+//   const game = new Game();
+//   const view = new View(game, el);
+    console.log("hello")
+});
+console.log("hello")
