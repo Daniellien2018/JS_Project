157e69808f3afc831429365d2802b4324dfb11ef
daniellien2018 <daniellien2018@gmail.com>
2022-08-14 22:49:43 -0700

    experiments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-// const View = require('./ttt-view.js')// require appropriate file
-// const Game = require('../ttt_node/game.js')// require appropriate file
-
 import {map} from './map';
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  map();
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   map();
+// });
