0c34c9b91620c1313e0d94bf7edb27ba7e400602
daniellien2018 <daniellien2018@gmail.com>
2022-08-15 12:16:30 -0700

    click-complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import { path } from 'd3';
+import { async } from 'regenerator-runtime';
 import {map} from './map';
-const d3 = await import("d3");
 
 // function UsStateChoropleth(data, {
 //     features = states,
@@ -11,7 +12,39 @@ const d3 = await import("d3");
 //     return Choropleth(data, {features, borders, width, height, ...options});
 //   }
 
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   map();
-// //   console.log("hi")
-// });
+document.addEventListener("DOMContentLoaded", (event) => {
+    map();
+});
+
+
+
+// const states = document.querySelectorAll("path")
+
+// states.forEach(state => {
+//     state.addEventListener('click', () =>{
+//         console.log('Hi')
+//         // console.log("states")
+//     })
+// })
+
+// const title = document.querySelector("h1")
+// title.addEventListener("click", () => {
+//     console.log("hi")
+// })
+
+// document.addEventListener("click", renderGraph);
+
+// function renderGraph(){
+
+// }
+// document.addEventListener("mouseover", showBox);
+// function showBox(){
+
+// }
+// path.addEventListener("click", e => {
+//     console.log(e.target)
+// })
+
+// states.addEventListener("click", e => {
+//     console.log(e.target)
+// })
--- src/map.js
+++ src/map.js
@@ -6,19 +6,32 @@ export const map = function(){
 
     var path = d3.geoPath();
 
-    d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+    var map = d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+
+        console.log(us)
+        console.log('HELLO')
+        
     if (error) throw error;
 
-    svg.append("g")
-        .attr("class", "states")
+    
+
+    svg.attr("class", "states")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.states).features)
         .enter().append("path")
-        .attr("d", path);
+        .attr("d", path)
+        .attr("data-id", el => el.id);
 
     svg.append("path")
         .attr("class", "state-borders")
-        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
+        ;
     });
 
+    const states = document.querySelector("svg")
+
+    states.addEventListener("click", (e) => {
+        console.log(e.target.dataset.id)
+    })
 }
+
