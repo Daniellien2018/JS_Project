3f2884f4cba4770a76b642b4fd35766bd737b74b
daniellien2018 <daniellien2018@gmail.com>
2022-08-17 14:25:17 -0700

    object-attempt

--- src/index.js
+++ src/index.js
@@ -6,3 +6,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
     map(2020);
 });
 
+//Side tabs
+function setupTabs() {
+    document.querySelectorAll(".tabs__button").forEach(button => {
+      button.addEventListener("click", () => {
+        const sideBar = button.parentElement;
+        const tabsContainer = sideBar.parentElement;
+        const tabsNumber = button.dataset.forTab;
+        const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNumber}"]`)
+
+        sideBar.querySelectorAll(".tabs__button").forEach( button => {
+          button.classList.remove("tabs__button--active");
+        });
+        tabsContainer.querySelectorAll(".tabs__content").forEach( tab => {
+          tab.classList.remove("tabs__content--active");
+        });
+
+        button.classList.add("tabs__button--active");
+        tabToActivate.classList.add("tabs__content--active");
+
+        // console.log(sideBar);
+        // console.log(tabsContainer);
+        // console.log(tabsNumber);
+        // console.log(tabToActivate);
+      });
+    });
+
+  }
+  document.addEventListener("DOMContentLoaded", () =>{
+    setupTabs();
+
+    document.querySelectorAll(".tabs").forEach(tabsContainer => {
+      tabsContainer.querySelector(".tabs__sidebar .tabs__button").click();
+    });
+  });
--- src/map.js
+++ src/map.js
@@ -1,41 +1,42 @@
 import data from "./data.json"
-console.log(data)
+// console.log(data)
 
 
-function renderSlider() {
-    const slider = document.createElement("div");
-    slider.setAttribute("id", "slider-container");
+// function renderSlider() {
+//     const slider = document.createElement("div");
+//     slider.setAttribute("id", "slider-container");
 
-    const sliderInput = document.createElement("input");
-    sliderInput.setAttribute("id", "year-slider");
-    sliderInput.setAttribute("type", "range");
-    sliderInput.setAttribute("min", "1999");
-    sliderInput.setAttribute("max", "2020");
-    // sliderInput.setAttribute("value", "2020");
-    sliderInput.setAttribute("step", "1");
+//     const sliderInput = document.createElement("input");
+//     sliderInput.setAttribute("id", "year-slider");
+//     sliderInput.setAttribute("type", "range");
+//     sliderInput.setAttribute("min", "1999");
+//     sliderInput.setAttribute("max", "2020");
+//     // sliderInput.setAttribute("value", "2020");
+//     sliderInput.setAttribute("step", "1");
 
-    const sliderLabel = document.createElement("span");
-    sliderLabel.setAttribute("id", "slider-current-year");
-    //   sliderLabel.innerHTML = 2020;
+//     const sliderLabel = document.createElement("span");
+//     sliderLabel.setAttribute("id", "slider-current-year");
+//     //   sliderLabel.innerHTML = 2020;
     
-    slider.appendChild(sliderInput);
-    slider.appendChild(sliderLabel);
-    document.getElementsByClassName("us-map-container")[0].appendChild(slider);
+//     slider.appendChild(sliderInput);
+//     slider.appendChild(sliderLabel);
+//     document.getElementsByClassName("us-map-container")[0].appendChild(slider);
 
-    document.getElementById("slider-current-year").style.left = `calc( 100% - 12.5px - ${document.getElementById("slider-current-year").offsetWidth / 2}px)`;
-}
+//     document.getElementById("slider-current-year").style.left = `calc( 100% - 12.5px - ${document.getElementById("slider-current-year").offsetWidth / 2}px)`;
+// }
 
 export const map = function(year){
     // let year = 2012
     var svg = d3.select("svg");
 
     var path = d3.geoPath();
+    // console.log(topojson.feature(us, us.objects.states).features)
 
     var map = d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-
-    if (error) throw error;
-    console.log(topojson.feature(us, us.objects.states).features)
-    svg.attr("class", "states")
+        if (error) throw error;
+        console.log(topojson.feature(us, us.objects.states).features)
+        
+        svg.attr("class", "states")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.states).features)
         .enter().append("path")
@@ -43,6 +44,7 @@ export const map = function(year){
         .attr("data-id", el => el.id)
         .attr("data-year", year)
         .attr("fill", (obj) => {
+            // console.log(obj)
             const rate = data.find(ele => +obj.id === ele["State Code"] && ele["Year"] === year)["Crude Rate"]
             if (rate === "unreliable"){
                 return "black"
@@ -56,37 +58,60 @@ export const map = function(year){
             }else{
                 return "navy"
             }
-        })
-        ;
-
-    svg.append("path")
+        });
+        svg.append("path")
         .attr("class", "state-borders")
-        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
-        ;
+        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
     });
 
     const states = document.querySelector("svg")
 
-    // states.addEventListener("mouseover", e => {
-    //     const name = e.target.__data__.properties.NAME;
-    //     const fullMessage = name.concat(": 33",);
-    //     const domEle = document.getElementById("hover-tooltip");
-    //     domEle.innerHTML = fullMessage;
-    //     domEle.style.opacity = 1;
-    // });
+    //tooltip
+    var tooltip = d3.select("#tooltip")
+    .append("div")
+    .style("position", "absolute")
+    .style("visibility", "hidden")
+    // .text("I'm a circle!")
+    // .text("path")
+    // .html(data)
+    .html( (obj) => {
+        // console.log(obj)
+        // console.log(data)
+        let resArr = data.filter(ele => ele["State Code"] === 6)
+        // console.log(resArr)?
+        // let resArr = data.filter(ele => ele["State Code"] === obj.id)
+        // console.log(data)
+        // console.log(obj)
+        // let res = data.find(ele => +obj.id === ele["State Code"])
+        // console.log(res)
+        // return res
+    })
+    // use .html, make text say what i want
+    .style("top", "-100px")
+    .style("left", "100px")
+    .style("border", "1px solid black")
+    .style("background", "white")
+    .style("border-radius", "10px");
 
-    // states.addEventListener("mouseleave", e => {
-    //     document.getElementById("hover-tooltip").innerHTML = "";
-    //     document.getElementById("hover-tooltip").style.opacity = 0;
-    // })
+    d3.select("svg")
+    .on("mouseover", (stateData) =>{
+        tooltip.style("visibility", "visible")
+        .style("top", event.pageY)
+        .style("left", event.pageX)
+    })
+    .on("mouseout", (stateData) => {
+        tooltip.style("visibility", "hidden")
+    })
 
+    //click
     states.addEventListener("click", (e) => {
         // all data points that match the state
         let resArr = data.filter(ele => ele["State Code"] === +e.target.dataset.id)
-        let state = resArr[0]["State"]
-
+        // console.log(e.target.dataset)
+        
         let population = 0;
         resArr.forEach (obj => {
+            // console.log(obj)
             population += obj["Population"]
         })
 
@@ -94,7 +119,7 @@ export const map = function(year){
         resArr.forEach (obj => {
             deaths += obj["Deaths"]
         })
-        
+                
         let crudeRate = 0;
         resArr.forEach (obj => {
             let ele = obj["Crude Rate"]
@@ -102,24 +127,24 @@ export const map = function(year){
                 crudeRate += obj["Crude Rate"]
             }
         })
-        console.log(deaths)
-        console.log(population)
-        console.log(crudeRate / resArr.length)
+
+        let state = resArr[0]["State"]
+
+        // console.log(deaths)
+        // console.log(population)
+        // console.log(crudeRate / resArr.length)
         // all data points that match the year
         let resArr2 = resArr.filter(ele => ele["Year"] === year)
-        console.log(state)
-        console.log(resArr2)
-        console.log(resArr2[0]["Crude Rate"])
-        
+        // console.log(state)
+        // console.log(resArr2)
+        // console.log(resArr2[0]["Crude Rate"])            
     })
 
+    //slider
     var slider = document.getElementById("myRange");
     var output = document.getElementById("value");
-
     output.innerHTML = slider.value
-
     slider.oninput = function(){
         output.innerHTML = this.value
     }
 }
-
