2d6a77f4395e65235a215eb35d6786d3e4265032
daniellien2018 <daniellien2018@gmail.com>
2022-08-18 10:24:27 -0700

    legend added

--- src/map.js
+++ src/map.js
@@ -1,6 +1,17 @@
 import data from "./data.json"
 // console.log(data)
 
+export function legend(){
+    var svg = d3.select(‘.map’);
+    svg.append(‘circle’).attr(‘cx’, 550).attr(‘cy’, 550).attr(‘r’, 10).style(‘fill’, ‘#F5EBEB’)
+    svg.append(‘circle’).attr(‘cx’, 550).attr(‘cy’, 580).attr(‘r’, 10).style(‘fill’, ‘#3AB1C8’)
+    svg.append(‘circle’).attr(‘cx’, 550).attr(‘cy’, 580).attr(‘r’, 10).style(‘fill’, ‘#2772DB’)
+    svg.append(‘circle’).attr(‘cx’, 550).attr(‘cy’, 580).attr(‘r’, 10).style(‘fill’, ‘#3AB1C8’)
+    svg.append(‘text’).attr(‘x’, 570).attr(‘y’, 553).text(‘z’).style(‘font-size’, ‘15px’).attr(‘alignment-baseline’, ‘middle’)
+    svg.append(‘text’).attr(‘x’, 570).attr(‘y’, 583).text(‘u’).style(‘font-size’, ‘15px’).attr(‘alignment-baseline’, ‘middle’)
+    svg.append(‘text’).attr(‘x’, 570).attr(‘y’, 583).text(‘z’).style(‘font-size’, ‘15px’).attr(‘alignment-baseline’, ‘middle’)
+    svg.append(‘text’).attr(‘x’, 570).attr(‘y’, 583).text(‘u’).style(‘font-size’, ‘15px’).attr(‘alignment-baseline’, ‘middle’)
+}
 
 // function renderSlider() {
 //     const slider = document.createElement("div");
